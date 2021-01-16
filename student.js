6e3bd80ce3546264f2dd82f59d0c83854896d90c
AMROAMRO404 <amro.amro1999@gmail.com>
2021-01-16 14:50:41 +0200

    add some validations and styling

--- student.js
+++ student.js
@@ -4,41 +4,41 @@ function showInput() {
     let name = document.getElementById("name");
     let id = document.getElementById("ID");
     let major = document.getElementById("major");
-    let student = {
-        "studentName": name.value,
-        "studenID": id.value,
-        "studenMajor": major.value
+    if (name.value && id.value && major.value) {
+        let student = {
+            "studentName": name.value,
+            "studenID": id.value,
+            "studenMajor": major.value
+        }
+        students.push(student);
+        let i = 0;
+        document.getElementById("myTable").innerHTML += "<tr>" + "<td>" + name.value + "</td>" + "<td>" + id.value + "</td>" + "<td>" + major.value + "</td>" + "</tr>"
+        i++;
+        document.getElementById("name").value = "";
+        document.getElementById("ID").value = "";
+    } else {
+        alert("Please fill all the fields")
     }
-    students.push(student);
-    let i = 0;
-    document.getElementById("display").innerHTML += "<li>" + "Name :" + name.value + "  id :" + id.value + " major :" + major.value + "</li>"
-    document.getElementById("myTable").innerHTML += "<tr>" + "<td>" + name.value + "</td>" + "<td>" + id.value + "</td>" + "<td>" + major.value + "</td>" + "</tr>"
-    i++;
-    document.getElementById("name").value = "";
-    document.getElementById("ID").value = "";
+
 }
-console.log("students.length");
-console.log(students.length);
 let searchName = document.getElementById("search");
 
 function search() {
-    document.getElementById("insearch").innerHTML = "";
-    console.log("students.length");
-    console.log(students.length);
-    console.log(students);
-    console.log(students[0].studentName);
-    console.log(searchName.value);
-    let reach = false;
-    for (let i = 0; i < students.length; i++) {
-        if (searchName.value === students[i].studentName) {
-            console.log("yesss");
-            document.getElementById("insearch").innerHTML += "Name :" + students[i].studentName + "  id :" + students[i].studenID + " major :" + students[i].studenMajor;
-            reach = true;
+    if (students.length != 0) {
+        let reach = false;
+        for (let i = 0; i < students.length; i++) {
+            if (searchName.value === students[i].studentName) {
+                document.getElementById("insearch").innerHTML += "Name :" + students[i].studentName + "  id :" + students[i].studenID + " major :" + students[i].studenMajor;
+                reach = true;
+            }
         }
+        if (reach === false) {
+            alert("No result !!")
+        }
+    } else {
+        alert("No students !!")
     }
-    if (reach === false) {
-        document.getElementById("insearch").innerHTML = "No result !!! ";
-    }
+
 }
 //Reference : https://www.w3schools.com/howto/howto_js_filter_table.asp
 function dynamicnSearch() {
@@ -61,17 +61,45 @@ function dynamicnSearch() {
 }
 
 function sortMe() {
-    document.getElementById("my-table-is-sorted").innerHTML = "";
-    students.sort((a, b) => (a.studentName > b.studentName) ? 1 : -1);
-    for (let i = 0; i < students.length; i++) {
-        document.getElementById("my-table-is-sorted").innerHTML += "<tr>" + "<td>" + students[i].studentName + "</td>" + "<td>" + students[i].studenID + "</td>" + "<td>" + students[i].studenMajor + "</td>" + "</tr>";
+    if (students.length != 0) {
+        document.getElementById("my-table-is-sorted").innerHTML =
+            `
+        <table class="table table-striped table-hover" id="my-table-is-sorted-reverse">
+            <tr class="header">
+                <th style="width:33%;">Name</th>
+                <th style="width:33%;">ID</th>
+                <th style="width:33%;">Major</th>
+            </tr>
+        </table>
+        `;
+        students.sort((a, b) => (a.studentName > b.studentName) ? 1 : -1);
+        for (let i = 0; i < students.length; i++) {
+            document.getElementById("my-table-is-sorted").innerHTML += "<tr>" + "<td>" + students[i].studentName + "</td>" + "<td>" + students[i].studenID + "</td>" + "<td>" + students[i].studenMajor + "</td>" + "</tr>";
+        }
+    } else {
+        alert("No students !!")
     }
 }
 
 function sortMeInReverse() {
-    document.getElementById("my-table-is-sorted-reverse").innerHTML = "";
-    students.sort((a, b) => (a.studentName < b.studentName) ? 1 : -1);
-    for (let i = 0; i < students.length; i++) {
-        document.getElementById("my-table-is-sorted-reverse").innerHTML += "<tr>" + "<td>" + students[i].studentName + "</td>" + "<td>" + students[i].studenID + "</td>" + "<td>" + students[i].studenMajor + "</td>" + "</tr>";
+    if (students.length != 0) {
+        document.getElementById("my-table-is-sorted-reverse").innerHTML =
+            `
+        <table class="table table-striped table-hover" id="my-table-is-sorted-reverse">
+            <tr class="header">
+                <th style="width:33%;">Name</th>
+                <th style="width:33%;">ID</th>
+                <th style="width:33%;">Major</th>
+            </tr>
+        </table>
+        `;
+        students.sort((a, b) => (a.studentName < b.studentName) ? 1 : -1);
+        for (let i = 0; i < students.length; i++) {
+            document.getElementById("my-table-is-sorted-reverse").innerHTML += "<tr>" + "<td>" + students[i].studentName + "</td>" + "<td>" + students[i].studenID + "</td>" + "<td>" + students[i].studenMajor + "</td>" + "</tr>";
+
+        }
+    } else {
+        alert("No students !!")
     }
+
 }
